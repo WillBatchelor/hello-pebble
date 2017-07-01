4b3d4f6145802e7306f4dbabd8eeb60e6c9a7739
WillBatchelor <batchelorwd8@gmail.com>
2017-07-01 14:51:17 -0400

    taking care of errors.

--- src/c/hello.c
+++ src/c/hello.c
@@ -5,9 +5,9 @@ TextLayer *text_layer;
 
 void init () {
   window = window_create();
-  text_layer = text_layercreate(GRect(0, 0, 144, 40));
+  text_layer = text_layer_create(GRect(0, 0, 144, 40));
   text_layer_set_text(text_layer, "Hello, Pebble!");
-  layer_add_child(window_get_root_layer(window)
+  layer_add_child(window_get_root_layer(window),
                  text_layer_get_layer(textlayer));
   window_stack_push(window, true);
 }
