008d2122a0db35eb4fc5735927523a1eb8348669
WillBatchelor <batchelorwd8@gmail.com>
2017-07-01 15:11:38 -0400

    fixed more errors

--- src/c/hello.c
+++ src/c/hello.c
@@ -8,7 +8,7 @@ void init () {
   text_layer = text_layer_create(GRect(0, 0, 144, 40));
   text_layer_set_text(text_layer, "Hello, Pebble!");
   layer_add_child(window_get_root_layer(window),
-                 text_layer_get_layer(textlayer));
+                 text_layer_get_layer(text_layer));
   window_stack_push(window, true);
 }
 
@@ -18,7 +18,7 @@ void deinit() {
 } 
 
 int main() {
-	init{};
+	init();
 	app_event_loop();
 	deinit();
 	retuen 0;
